fdf7046a3006ef5ef3690be1990efae4d7f6cdc8
Halfdan <GamerHalfdan@gmail.com>
2024-03-14 20:47:15 +0100

    Updated some files for pypi

--- examples/all_functions.py
+++ examples/all_functions.py
@@ -1,4 +1,4 @@
-from chilicloud import *
+from chilicloud.chilicloud import *
 
 updateApiToken() # Gets a new X-Apigw-Session token for interacting with the API
 file_uuid = uploadFile('test_file.txt') # Uploads out test_file.txt to the server
